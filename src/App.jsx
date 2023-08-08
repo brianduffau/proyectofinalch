f176fa88a3766938ac213d898427a899023436a5
brianduffau <brianduffau@gmail.com>
2023-08-08 16:23:55 -0300

    PreEntrega1+ReparadoDuffau

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,6 @@ function App() {
     
     <ItemListContainer greeting="Bienvenido!" />
   
-  
-  
-  
   </div>
 
   )
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,8 +1,8 @@
 
 const ItemListContainer = ({greeting}) => {
     return (
-        <div className='container'>
-            <p>{greeting}</p>
+        <div className='container' style={{textAlign:"center", width:"1296px", height:"500px"}}>
+            <p style={{textAlign: "center", lineHeight: "250px"}}>{greeting}</p>
         </div>
     )
 }
